78336ae55c19563be1009eba1828de01c60884b8
Patrick Kim <patrick.yrkim@gmail.com>
2022-01-06 01:34:42 -0800

    Update sidebar styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <Router>
       <Navbar click={() => setSideToggle(true)} />
-      <SideDrawer show={sideToggle} />
+      <SideDrawer show={sideToggle} click={() => setSideToggle(false)} />
       <Backdrop show={sideToggle} click={() => setSideToggle(false)} />
 
       <main>
--- frontend/src/components/SideDrawer/SideDrawer.js
+++ frontend/src/components/SideDrawer/SideDrawer.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './SideDrawer.css'
 
-const SideDrawer = ({ show }) => {
+const SideDrawer = ({ show, click }) => {
     const sideDrawerClass = ['sidedrawer']
 
     if (show) {
@@ -9,7 +10,26 @@ const SideDrawer = ({ show }) => {
     }
 
     return (
-        <div className={sideDrawerClass.join(' ')}></div>
+        <div className={sideDrawerClass.join(' ')}>
+            <ul className='sidedrawer-links' onClick={click}>
+                <li>
+                    <Link to='/cart'>
+                        <i className='fas fa-shopping-cart'></i>
+                        <span>
+                            Cart
+                            <span className='sidedrawer-cartbadge'>
+                                0
+                            </span>
+                        </span>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/'>
+                        Shop
+                    </Link>
+                </li>
+            </ul>
+        </div>
     )
 }
 
